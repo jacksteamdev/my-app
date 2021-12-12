e44681542c5b1c1a863c255a53d6989b7669f642
Nathan Pearson <npearson72@gmail.com>
2021-12-13 00:55:15 +0200

    Introduces React into popup.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import './index.css'
-// import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
-
-const foo = document.getElementById('foo');
-
-foo.appendChild(document.createTextNode('Bar???'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
